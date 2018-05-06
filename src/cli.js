f7245d82fff43df66aa1176d68e651d0c34f881a
hagb4rd <hagb4rd@gmail.com>
2018-05-06 19:12:45 +0200

    cli.js fixed

--- src/cli.js
+++ src/cli.js
@@ -7,7 +7,7 @@ var ini = require('ini');
 process.stdin                        //connect streams together with `pipe`
   .pipe(es.split())                  //split stream to break on newlines
   .pipe(es.map(function (data, cb) { //turn this async function into a stream
-    cb(null, remap(data)[0]+'\n')          //render it nicely
+    cb(null, mapper.remap(data)[0]+'\n')          //render it nicely
   }))
   .pipe(es.wait(function (err, body) {
     console.log(JSON.stringify(body.split('\n').slice(0,-2)));
