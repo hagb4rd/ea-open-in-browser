6c5a904c20e1c159e5d1c2d0b2118b2023be0866
hagb4rd <hagb4rd@gmail.com>
2018-05-07 02:22:31 +0200

    blah

--- src/remap.js
+++ src/remap.js
@@ -9,6 +9,10 @@ var mapper=(typeof(module)!='undefined'?module.exports:{});
 var protocol=mapper.protocol='https';
 
 //mapping
+var fallback={};
+fallback["D:\\www"]="//iis";
+fallback["G:\\Bilder"]="//iis/Bilder";
+fallback["F:\\videos\\porn"]="//iis/porn";
 
 
 var reduce=m=>x=>m.reduce((aggr,[k,v])=>aggr.replace(k,v),x);
@@ -19,15 +23,12 @@ var remap=mapper.remap=(li,m=mapping)=>(li=[...(Array.isArray(li)?li:[li])],m=Ob
 var csv=mapper.csv=(file, row=/\r\n/, col=/\t+/)=>fread(path.resolve(file)).split(row).map(line=>(col?line.split(col):line));  
 var fread=mapper.fread=(file)=>fs.readFileSync(path.resolve(file), {encoding:"utf8"}); 
 var fwrite=mapper.fwrite=(file,data)=>fs.readWriteSync(path.resolve(file), (typeof(data)=="string"?data:JSON.stringify(data)), {encoding:"utf8"}); 
-var loadini=mapper.loadini=()=>require('ini').parse(require('fs').readFileSync(path.resolve('./remap.ini'),{encoding:'utf8'}))
+var loadini=mapper.loadini=(file=path.resolve(__dirname,"../remap.ini"))=>require('ini').parse(require('fs').readFileSync(file),{encoding:'utf8'});
 
-/*
-var o={};
-o["D:\\www"]="//iis";
-o["G:\\Bilder"]="//iis/Bilder";
-o["F:\\videos\\porn"]="//iis/porn";
-/* */
 
-var mapping=mapper.mapping=loadini(path.resolve(__dirname,'../remap.ini'));
-
-//var fwrite=(file,obj)=>require('fs').writeFileSync(require("path").resolve(__dirname,file), JSON.stringify(obj), {encoding:'utf8'});
+//enough for today
+try {
+  var mapping=mapper.mapping=loadini()||fallback;
+} catch(e) {
+  var mapping=mapper.mapping=fallback;
+}
